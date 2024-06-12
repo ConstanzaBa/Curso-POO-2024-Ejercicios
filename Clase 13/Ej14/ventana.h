37d2bc04b88a2e32b8c7b3645f66da44b085befe
Constanza Baigorria <121911671+ConstanzaBa@users.noreply.github.com>
2024-06-12 17:18:11 -0300

    Add files via upload

---
+++ Clase 13/Ej14/ventana.h
@@ -0,0 +1,25 @@
+#ifndef VENTANA_H
+#define VENTANA_H
+
+#include <QWidget>
+
+namespace Ui { class Ventana; }
+
+class Ventana : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Ventana(QWidget *parent = nullptr);
+    ~Ventana();
+
+private:
+    Ui::Ventana *ui;
+
+private slots:
+    void slot_volver();
+
+signals:
+    void signal_volver();
+};
+#endif // VENTANA_H
